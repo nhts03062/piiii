d5afa39bdf11669d8524bdfcd6c0fe3c5d1fb905
Thai Son <nhts030627@gmail.com>
2024-05-19 03:34:22 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  introTitle: "Pi à!",
+  introDesc: `Sao giờ Pi chưa ngủ , Pi có muốn thành ngừ đẹp không ???`,
+  btnIntro: "yes",
+  title: "Phải chăng Pi đã uống 10 gói cafe !!!!!!!",
+  desc: "Pi thức khuya vậy muốn thành ngừ đẹp hay ngừ xấu???",
+  btnYes: "Ngừ xấu !!!!!",
+  btnNo: "Ngừ đẹp )))",
+  question: "Đùa thoi điền đáp án dô đây !!!!!!",
+  btnReply: "Gửi cho Pi <3",
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Gáng mà tập ngủ sớm 🥰. Học bài ôn thi nx 😘😘",
+  messDesc: "Ngủ ngon nhá Pi, Mơ đẹp !!!! ",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "http://fb.com", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
